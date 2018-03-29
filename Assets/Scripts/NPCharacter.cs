8ee0e85021af7f1fb950f9ea5019fbea65803196
bobbykemp <robert.kemp@mavs.uta.edu>
2018-03-29 14:36:01 -0500

    editing npcharacter follow routine
    follow routine now keeps the npc from getting too far or too close, but it is buggy

--- Assets/Scripts/NPCharacter.cs
+++ Assets/Scripts/NPCharacter.cs
@@ -16,23 +16,23 @@ using UnityEngine;
 public class NPCharacter : Character
 {
     /* Public Variables */
-    public float maxFollowDistance = 1; // How close will our NPC follow player?
-	public float minFollowDistance = 1;
+	public float minFollowDistance = 10; //set this to something ~10 in the edito - this is the minimum distance maintained between player and npc
+    public float youCanOnlyBeThisFarAway = 5; //
 	public float movementScalingFactor = 4.5f; //character movement was too slow
 	public GameObject target;
 
     /* Getter and Setter */
-    public float MaxFollowDist
-    {
-		get { return maxFollowDistance; }
-		set { maxFollowDistance = value; }
-    }
-
 	public float MinFollowDist
 	{
 		get { return minFollowDistance; }
 		set { minFollowDistance = value; }
 	}
+
+    public float MaxFollowDist
+    {
+        get { return minFollowDistance; }
+        set { youCanOnlyBeThisFarAway = value; }
+    }
 
     /* Unity Functions */
     public override void Awake()
@@ -41,26 +41,38 @@ public class NPCharacter : Character
         Chartype = CharType.NPC; // Need to make sure our NPC is N.P.C!
     }
 
-    public virtual void Update()
+    public virtual void FixedUpdate()
     {
-		Follow (target.transform);
-
+		Follow (target);
     }
-
+    
     /* Functions */
-    public virtual void Follow(Transform target) // Its virtual. If Enemy class have more complex function, override it.
+    public virtual void Follow(GameObject target) // Its virtual. If Enemy class have more complex function, override it.
     {
-        Vector2 dist = transform.position - target.position; // How far am I?
-		int dir = 1;
+        Vector2 dist = transform.position - target.transform.position; // How far am I?
+        Vector2 targetSpeed = target.GetComponent<Rigidbody2D>().velocity;
+        int dir = 1;
 
-		if (dist.magnitude > MinFollowDist) { // Am I too far from target?
+        //Debug.Log("Distance from target = " + dist);
+        Debug.Log("Velocity =" + targetSpeed);
+
+		if (dist.magnitude > MinFollowDist) // Am I too far from target?
+        { 
 			dir = dist.x > 0 ? -1 : 1; // Set direction based on which side of target I'm on (now facing toward target)
-			Debug.Log ("too far from target");
+			//Debug.Log ("too far from target");
 			Vector2 vel = new Vector2 (H_CurrSpeed, body.velocity.y); // Update velocity profile
-			body.velocity = vel * dir * movementScalingFactor; // Set velocit	y to our character
-		} else {
-			body.velocity = new Vector2(0, body.velocity.y);
+			body.velocity = vel * dir * movementScalingFactor; // Set velocity to our character
 		}
+        else if(dist.magnitude <= MinFollowDist - 1)
+        {
+            dir = dist.x > 0 ? 1 : -1; // Set direction based on which side of target I'm on (now facing toward target)
+            //Debug.Log("too close to target");
+            Vector2 vel = new Vector2(H_CurrSpeed, body.velocity.y); // Update velocity profile
+            body.velocity = vel * dir * movementScalingFactor; // Set velocity to our character
+        }
+       // else {
+		//	body.velocity = new Vector2(0, body.velocity.y);
+	//	}
 
     }
 
